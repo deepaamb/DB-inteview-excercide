adc15d2e25d32ac01f2b089682041cb8d8f5b3b2
deepaamb <73662188+deepaamb@users.noreply.github.com>
2021-05-18 11:37:03 +0100

    First commit

--- src/main/java/com/acme/mytrader/price/impl/PriceSourceImpl.java
+++ src/main/java/com/acme/mytrader/price/impl/PriceSourceImpl.java
@@ -3,19 +3,12 @@ package com.acme.mytrader.price.impl;
 import com.acme.mytrader.price.PriceListener;
 import com.acme.mytrader.price.PriceSource;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class PriceSourceImpl implements PriceSource {
     List<PriceListener> priceListenerList = new CopyOnWriteArrayList<>();
 
-    private int excitingCorpStockPrice = 100;
-
-    public void changeExcitingCorpStockPrice(String security, double price){
-        excitingCorpStockPrice += price;
-        notifyPriceUpdate(security,excitingCorpStockPrice);
-    }
 
     @Override
     public void addPriceListener(PriceListener listener) {
